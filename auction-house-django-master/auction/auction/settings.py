183ce4da42390d8bdbf9c6822d2e6c7a15f5f536
Aryaman <aryavnps@gmail.com>
2020-04-01 00:21:09 +0530

    Added models, views, templates, migrations

--- auction-house-django-master/auction/auction/settings.py
+++ auction-house-django-master/auction/auction/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auction_house',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Media directory stores the images uploaded by the users
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')      #Server path to store files in the computer
+MEDIA_URL = '/media/' 
--- auction-house-django-master/auction/auction_house/models.py
+++ auction-house-django-master/auction/auction_house/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    """
+    Model containing different attributes required.
+    """
+    name = models.CharField(max_length=100,verbose_name="Name")                     #Name of the auctioned product
+    description = models.CharField(max_length=1000,verbose_name="Description")      #Short description of the auctioned product
+    starting_bid = models.CharField(max_length=10,verbose_name="Starting bid")      #Starting bid of the auctioned product
+    deadline = models.DateField(verbose_name="Deadline")                            #Deadline for auctioning of the product
+    contact = models.CharField(max_length=13,verbose_name="Mobile Number")          #Contact of the owner of the product
+    image = models.ImageField(upload_to='images/')                                  #Image of the product to be auctioned
+
+    def __str__(self):
+        """
+        Method to return the name of the product.
+        """
+        return self.name
